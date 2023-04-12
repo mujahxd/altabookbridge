16bca60a6becdfdaf9eb70d620d056b98c470811
Mujahid <mujahid.170997@gmail.com>
2023-04-13 06:58:06 +0700

    update register user

--- app/features/user/handler.go
+++ app/features/user/handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/mujahxd/altabookbridge/app/features/user/data"
+	"github.com/mujahxd/altabookbridge/helper"
 )
 
 type handler struct {
@@ -23,6 +24,11 @@ func (h *handler) RegisterUser() echo.HandlerFunc {
 			c.Logger().Error("error bind: ", err.Error())
 			return c.JSON(http.StatusBadRequest, nil)
 		}
-		return c.JSON(http.StatusOK, nil)
+		user, err := h.service.RegisterUser(Core(input))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, nil)
+		}
+		response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+		return c.JSON(http.StatusOK, response)
 	}
 }
