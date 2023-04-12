b8add165c3fe7b47b533a2d925dbe66284550570
Mujahid <mujahid.170997@gmail.com>
2023-04-13 00:32:03 +0700

    update user routes

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -2,9 +2,13 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/mujahxd/altabookbridge/app/features/user"
 )
 
 func InitRoute(e *echo.Echo, h user.Handler) {
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
 	e.POST("/users", h.RegisterUser())
 }
