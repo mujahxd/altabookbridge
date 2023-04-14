a2b695e63ce79dda1a3b9bc4800d7f690a2b56f0
Kristain Putra <kputrapar@gmail.com>
2023-04-14 09:45:14 +0700

    fix cloudinary env (error in server)

--- config/load_env.go
+++ config/load_env.go
@@ -28,11 +28,11 @@ func InitConfig() *Config {
 var TokenSecret string
 
 func readConfig() *Config {
-	// err := godotenv.Load(".env")
-	// if err != nil {
-	// 	log.Fatal("Cannot read config variable")
-	// 	return nil
-	// }
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Cannot read config variable")
+		return nil
+	}
 	var result = new(Config)
 	result.DBUser = os.Getenv("DBUser")
 	result.DBPassword = os.Getenv("DBPassword")
